47d476d69d98c1829ae11cc5854ac4951ef12807
ajesh2014 <ajeshvaghela@live.com>
2017-03-27 20:34:33 +0100

    adding seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,34 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'username' => str_random(10),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('ajesh'),
+            'firstname' => 'firstname',
+            'lastname' => 'lasttname',
+            'usertype_id' => 1,
+        ]);
+
+        DB::table('address')->insert([
+            'user_id' => 1,
+            'company' => str_random(10),
+            'addressline1' => str_random(30),
+            'addressline2' => str_random(30),
+            'city' => str_random(10),
+            'postcode' => str_random(6),
+            'country' => str_random(10),
+            'telephone1' => str_random(11),
+            'telephone2' => str_random(11),
+            'is_default' => true,
+        ]);
+
+        DB::table('user_types')->insert([
+            'type' => 'customer',
+        ]);
+
+         DB::table('user_types')->insert([
+            'type' => 'contractor',
+        ]);
     }
 }

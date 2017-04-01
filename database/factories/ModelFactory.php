d3e0afd142bf67d3b794c5485e35e9519006b41b
ajesh2014 <ajeshvaghela@live.com>
2017-04-01 21:38:29 +0100

    testing model definitions

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,12 +13,64 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(App\User::class, function (Faker\Generator $faker) {
-    static $password;
-
     return [
-        'name' => $faker->name,
+        'username' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+        'firstname'=> $faker->firstname,
+        'lasttname'=> $faker->lastname,
+        'usertype_id' => 1
     ];
-});
+}); 
+
+$factory->define(App\Address::class, function (Faker\Generator $faker) {
+
+    return [
+        'user_id' =>  1,
+        'company' => str_random(20),
+        'addressline1' => $faker->address,
+        'addressline2' =>$faker->streetAddress,
+        'city' => $faker->postCode,
+        'country' =>$faker->country,
+        'telephone1' => $faker->phone,
+        'telephone2' => $faker->phone,
+        'is_default'=> true
+    ];
+});   
+
+
+$factory->define(App\Job::class, function (Faker\Generator $faker) {
+
+    return [
+        'user_id' => 1,
+        'title' => str_random(20),
+        'description' => str_random(100),
+        'trade_id' => 1,
+        'address_id' => function () {
+            return factory(App\Address::class)->create()->id;
+        },
+    ];
+});    
+
+
+$factory->define(App\Quote::class, function (Faker\Generator $faker) {
+ 
+    return [
+        'user_id' => 1,
+        'job_id' =>  1,
+        'description' => str_random(100),
+        'time' => 2,
+        'rate' => 65,
+        'estimate' => 154,
+        'status' => 0
+    ];
+}); 
+
+$factory->define(App\Media::class, function (Faker\Generator $faker) {
+
+    return [
+        'job_id' => 1,
+        'filename' => image('/tmp', 200, 300, 'cats', false)
+    ];
+}); 
